794524bfc2575f685962e0d6e22118c60ced5519
Davide Gallotti <d.gallotti@outlook.it>
2022-11-08 00:29:21 +0100

    Add TileMap with basic tile generation

--- Assets/Scripts/Blocks/BaseBlock.cs
+++ Assets/Scripts/Blocks/BaseBlock.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BaseBlock : MonoBehaviour, Breakable {
     
+    //public Tile tile;
     public Timer timer;
+    public Tile tile;
+
+    public BaseBlock() {
+        tile = new Tile();
+        Texture2D texture = Resources.Load<Texture2D>("Tiles/block") as Texture2D;
+        tile.sprite = Sprite.Create(texture,
+            new Rect(0, 0,512, 512), // section of texture to use
+            new Vector2(0.5f, 0.5f), // pivot in centre
+            512, // pixels per unity tile grid unit
+            1,
+            SpriteMeshType.Tight,
+            Vector4.zero);
+    }
 
     void Start() {
-        Debug.Log("Spawn");
+        
     }
 
     void Update() {
-        if (Input.GetKey("a"))
-        {
-            onBreak();
-        }
+        Debug.Log("PROVA");
     }
 
     public void onBreak() {
--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Map : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        var tileMap = this.GetComponent<Tilemap>();
+        for (var i = 0; i < 10; i++)
+        {
+            for (var j = 0; j < 10; j++)
+            {   
+                var block = new BaseBlock();
+                tileMap.SetTile(new Vector3Int(i,j,0),block.tile);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Break(int axis_x, int axis_y)
+    {
+
     }
 }
